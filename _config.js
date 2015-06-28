48e9dfb07e10ef0add6ac24cd32009cde0dbca54
Bryan Abrams <bryan.abrams.eu@gmail.com>
2015-06-28 15:31:57 +0200

    Removed my telegram group specific development and added option for setting the webhook via the bot.

--- _config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  token: '@BotFather on telegram will give you this token when you create a bot.',
-  key: '/etc/ssl/path_to_key.key',
-  cert: '/etc/ssl/path_to_cert.pem',
-  port: 443
-}
-
