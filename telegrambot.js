59b9072854842a47abe947ec804d16e304c42cf5
Bryan Abrams <bryan.abrams.eu@gmail.com>
2015-06-28 16:57:42 +0200

    Added functions for handling different updates

--- telegrambot.js
+++ telegrambot.js
@@ -6,7 +6,37 @@ var promise = require('promise');
 var TelegramBot = function( token ) {
   this.api_url           = 'https://api.telegram.org/bot' + token;
   this.command_character = '/';
-  this.commands          = {};
+  this.commands  = {};
+  // Default Functions for logging the actions, override with your own functions
+  this.functions = {
+    onPlainText:       function( id, from, chat, date, message )  { console.log( "onPlainText", id, from, chat, date, message ); },
+    onAudio:           function( id, from, chat, date, audio )    { console.log( "onAudio", audio ); },
+    onDocument:        function( id, from, chat, date, document ) { console.log( "onDocument", document ); },
+    onPhoto:           function( id, from, chat, date, photo )    { console.log( "onPhoto", photo ); },
+    onSticker:         function( id, from, chat, date, sticker )  { console.log( "onSticker", sticker ); },
+    onVideo:           function( id, from, chat, date, video )    { console.log( "onVideo", video ); },
+    onContact:         function( id, from, chat, date, contact )  { console.log( "onContact", contact ); },
+    onLocation:        function( id, from, chat, date, location ) { console.log( "onLocation", location ); },
+    onNewParticipant:  function( id, from, chat, date, user )     { console.log( "onNewParticipant", user ); },
+    onLeftParticipant: function( id, from, chat, date, user )     { console.log( "onLeftParticipant", user ); },
+  };
+  this.ready = false;
+  this.bot_id = -1;
+  this.bot_name = "uninitialized";
+  
+  var self = this;
+  this.getMe().then( function(body) { self.setReady(body) }, console.log );
+}
+
+/*
+ * When created this bot does a 'getMe' to get details on the robot
+ */
+TelegramBot.prototype.setReady = function( body ) {
+  if ( body.ok == true ) {
+    this.bot_id   = body.result.id;
+    this.bot_name = body.result.username;
+    this.ready    = true;
+  }
 }
 
 /*
@@ -15,10 +45,60 @@ var TelegramBot = function( token ) {
  * if the updated message text contains a command.
  */
 TelegramBot.prototype.onUpdate = function( message ) {
-  if ( this.isChat(message) && this.isCommand(message) ) {
-    this.onCommand( message.from, message.chat, this.parseArguments(message.text) );
+  var id   = message.message_id;
+  var from = message.from;
+  var date = message.date;
+  var chat = message.chat;
+  if ( this.ready == true ) {
+    if ( this.isChat(message) && this.isCommand(message) ) {
+      this.onCommand( id, from, chat, date, this.parseArguments(message.text) );
+    }
+    else {
+      if ( this.isChat(message) ) {
+        this.functions.onPlainText( id, from, chat, date, message.text );
+      }
+      else if ( message.audio != undefined ) {
+        this.functions.onAudio( id, from, chat, date, message.audio );
+      }
+      else if ( message.document != undefined ) {
+        this.functions.onDocument( id, from, chat, date, message.document );
+      }
+      else if ( message.photo != undefined ) {
+        this.functions.onPhoto( id, from, chat, date, message.photo );
+      }
+      else if ( message.sticker != undefined ) {
+        this.functions.onSticker( id, from, chat, date, message.sticker );
+      }
+      else if ( message.video != undefined ) {
+        this.functions.onVideo( id, from, chat, date, message.video );
+      }
+      else if ( message.contact != undefined ) {
+        this.functions.onContact( id, from, chat, date, message.contact );
+      }
+      else if ( message.location != undefined ) {
+        this.functions.onLocation( id, from, chat, date, message.location );
+      }
+      else if ( message.new_chat_participant != undefined ) {
+        this.functions.onNewParticipant( id, from, chat, date, message.new_chat_participant );
+      }
+      else if ( message.left_chat_participant != undefined ) {
+        this.functions.onLeftParticipant( id, from, chat, date, message.left_chat_participant );
+      } 
+    }
   }
 }
+
+/*
+ * Runs the specified command if registered. Name is excluding the / character. 
+ */
+TelegramBot.prototype.onCommand = function( id, from, chat, date, args ) {
+  var run = this.commands[args[0]];
+  if ( run != undefined ) 
+    run( id, from, chat, date, args.splice( 1, args.length ) );
+}
+
+/*
+ *
 
 /*
  * Returns true if the first character of the message text is a '/'
@@ -58,17 +138,6 @@ TelegramBot.prototype.addCommand = function( name, callback ) {
 }
 
 /*
- * Searches & executes a command loaded from a module and returns a promise
- * fulfill is called if the command was found and processed succesfully
- * reject is called if the command doesn't exist or errors occured during execution
- */
-TelegramBot.prototype.onCommand = function( from, chat, args ) {
-  var run = this.commands[args[0]];
-  if ( run == undefined ) return;
-  run( from, chat, args.splice( 1, args.length ) );
-}
-
-/*
  * Given a map where the key is the parameter and the value is the argument
  * Build up a URL for calling a service
  */
@@ -95,7 +164,7 @@ TelegramBot.prototype.httpsRequest = function( url ) {
   return new promise( function(fulfill, reject) {
     request( url, function( error, response, body ) {
       if(error) reject(error);
-      else fulfill(body);
+      else fulfill(JSON.parse(body));
     });
   });
 }
@@ -155,6 +224,19 @@ TelegramBot.prototype.sendPhoto = function( chat_id, photo, caption, reply_to_me
   if ( reply_markup != undefined ) 
     params.reply_markup = reply_markup;
   return this.httpsRequest( this.buildURI( 'sendPhoto', params ) ); 
+}
+
+/*
+ * https://core.telegram.org/bots/api#sendsticker
+ * Use this method to send .webp stickers.
+ */
+TelegramBot.prototype.sendSticker = function( chat_id, sticker, reply_to_message_id, reply_markup ) {
+  var params = { chat_id: chat_id, sticker: sticker };
+  if ( reply_to_message_id != undefined ) 
+    params.reply_to_message_id = reply_to_message_id;
+  if ( reply_markup != undefined ) 
+    params.reply_markup = reply_markup;
+  return this.httpsRequest( this.buildURI( 'sendSticker', params ) );
 }
 
 /*
