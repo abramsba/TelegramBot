33cd394688e2c474e1c3fbfc8b8c337d6a67115e
Bryan Abrams <bryan.abrams.eu@gmail.com>
2015-06-28 14:09:48 +0200

    Reworked URI building, added all possible API options as of 28.06.2015

--- telegrambot.js
+++ telegrambot.js
@@ -15,8 +15,6 @@ var TelegramBot = function( token ) {
  * if the updated message text contains a command.
  */
 TelegramBot.prototype.onUpdate = function( message ) {
-  //console.log( "chat?", this.isChat(message) );
-  //console.log( "command?", this.isCommand(message) );
   if ( this.isChat(message) && this.isCommand(message) ) {
     this.onCommand( message.from, message.chat, this.parseArguments(message.text) );
   }
@@ -71,20 +69,132 @@ TelegramBot.prototype.onCommand = function( from, chat, args ) {
 }
 
 /*
- * Sends a HTTPS GET request and returns a promise if successful 
+ * Given a map where the key is the parameter and the value is the argument
+ * Build up a URL for calling a service
  */
-TelegramBot.prototype.sendMessage = function( chat_id, text ) {
-  var uri_chat = 'chat_id=' + encodeURIComponent(chat_id);
-  var uri_text = 'text=' + encodeURIComponent(text);
-  var url = this.api_url + '/sendMessage?' + uri_chat + '&' + uri_text; 
+TelegramBot.prototype.buildURI = function( resource, params ) {
+  var param_keys = params != undefined ? _.keys(params) : [];
+  var uri_resource = this.api_url + '/' + resource;
+  var uri_params = "";
+  _.each( param_keys, function(key, index) {
+    var uri_param = key + '=' + encodeURIComponent( params[key] );
+    if ( index != param_keys.length - 1 ) 
+      uri_param += "&";
+    uri_params += uri_param;
+  });
+  if ( uri_params.length != 0 ) {
+    uri_resource += "?" + uri_params
+  }
+  return uri_resource;
+}
+
+/*
+ * Send a HTTPS request and return a promise
+ */
+TelegramBot.prototype.httpsRequest = function( url ) {
   return new promise( function(fulfill, reject) {
     request( url, function( error, response, body ) {
-      if(error)
-        reject(error);
-      else
-        fulfill(body);
+      if(error) reject(error);
+      else fulfill(body);
     });
   });
 }
 
+/*
+ * https://core.telegram.org/bots/api#getme
+ * A simple method for testing your bot's auth token. 
+ */
+TelegramBot.prototype.getMe = function() {
+  var url = this.buildURI( 'getMe' );
+  return this.httpsRequest(url);
+}
+
+/*
+ * https://core.telegram.org/bots/api#sendmessage
+ * Use this method to send text messages. On success, the sent Message is returned. 
+ */
+TelegramBot.prototype.sendMessage = function( chat_id, text, disable_web_page_preview, reply_to_message_id, reply_markup ) {
+  var params = { chat_id: chat_id, text: text };
+  if ( disable_web_page_preview != undefined ) 
+    params.disable_web_page_preview = disable_web_page_preview;
+  if ( reply_to_message_id != undefined ) 
+    params.reply_to_message_id = reply_to_message_id;
+  if ( reply_markup != undefined ) 
+    params.reply_markup = reply_markup;
+  return this.httpsRequest( this.buildURI( 'sendMessage', params ) );
+}
+
+/*
+ * https://core.telegram.org/bots/api#forwardmessage
+ * Use this method to forward messages of any kind. On success, the sent Message is returned.
+ */
+TelegramBot.prototype.forwardMessage = function( chat_id, from_chat_id, message_id ) {
+  var params = { chat_id: chat_id, from_chat_id: from_chat_id, message_id: message_id };
+  return this.httpsRequest( this.buildURI( 'forwardMessage', params ) );
+}
+
+/*
+ * https://core.telegram.org/bots/api#sendphoto
+ * Use this method to send photos. On success, the sent Message is returned.
+ */
+TelegramBot.prototype.sendPhoto = function( chat_id, photo, caption, reply_to_message_id, reply_markup ) {
+  var params = { chat_id: chat_id, photo: photo };
+  if ( caption != undefined ) 
+    params.caption = caption;
+  if ( reply_to_message_id != undefined ) 
+    params.reply_to_message_id = reply_to_message_id;
+  if ( reply_markup != undefined ) 
+    params.reply_markup = reply_markup;
+  return this.httpsRequest( this.buildURI( 'sendPhoto', params ) ); 
+}
+
+/*
+ * https://core.telegram.org/bots/api#sendaudio
+ * Use this method to send audio files, if you want Telegram clients to display the file as a playable voice message. For this to work, your audio must be in an .ogg file encoded with OPUS
+ */
+TelegramBot.prototype.sendAudio = function( chat_id, audio, reply_to_message_id, reply_markup ) {
+  var params = { chat_id: chat_id, audio: audio };
+  if ( reply_to_message_id != undefined ) 
+    params.reply_to_message_id = reply_to_message_id;
+  if ( reply_markup != undefined )
+    params.reply_markup = reply_markup;
+  return this.httpsRequest( this.buildURI( 'sendAudio', params ) );
+}
+
+/*
+ * https://core.telegram.org/bots/api#senddocument
+ * Use this method to send general files. On success, the sent Message is returned. Bots can currently send files of any type of up to 50 MB in size
+ */
+TelegramBot.prototype.sendDocument = function( chat_id, document, reply_to_message_id, reply_markup ) {
+  var params = { chat_id: chat_id, document: document };
+  if ( reply_to_message_id != undefined )
+    params.reply_to_message_id = reply_to_message_id;
+  if ( reply_markup != undefined )
+    params.reply_markup = reply_markup;
+  return this.httpsRequest( this.buildURI( 'sendDocument', params ) );
+}
+
+/*
+ * https://core.telegram.org/bots/api#sendvideo
+ * Use this method to send video files, Telegram clients support mp4 videos (other formats may be sent as Document).
+ */
+TelegramBot.prototype.sendVideo = function( chat_id, video, reply_to_message_id, reply_markup ) {
+  var params = { chat_id: chat_id, video: video };
+  if ( reply_to_message_id != undefined ) 
+    params.reply_to_message_id = reply_to_message_id;
+  if ( reply_markup != undefined )
+    params.reply_markup = reply_markup;
+  return this.httpsRequest( this.buildURI( 'sendVideo', params ) );
+}
+
+/*
+ * https://core.telegram.org/bots/api#sendchataction
+ * Use this method when you need to tell the user that something is happening on the bot's side.
+ */
+TelegramBot.prototype.sendChatAction = function( chat_id, action ) {
+  var params = { chat_id: chat_id, action: action };
+  return this.httpsRequest( this.buildURI( 'sendChatAction', params ) );
+}
+
 module.exports = TelegramBot;
+
